2282de07d2177ccc7ee0dd89801a8b5c3c1a56af
Yvonne Gitata <yvonne.gitata@twiga.com>
2024-08-14 15:06:40 +0300

    dynamic

--- arrays_slices_maps/lists.go
+++ arrays_slices_maps/lists.go
@@ -3,13 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	productNames := [5]string{"Laptop", "Mouse", "Keyboard", "Monitor", "CPU"}
-	prices := [4]float64{100.30, 200.10, 300.99, 400.60}
-	fmt.Println(prices)
-	fmt.Println(productNames)
-	fmt.Println(prices[0])
+	prices := []float64{10.77, 22.43}
 
-	// Slicing
-	featuredPrices := prices[1:3]
-	fmt.Println(featuredPrices)
+	prices = append(prices, 33.33)
+	fmt.Println(prices)
 }
+
+// func main() {
+// 	productNames := [5]string{"Laptop", "Mouse", "Keyboard", "Monitor", "CPU"}
+// 	prices := [4]float64{100.30, 200.10, 300.99, 400.60}
+// 	fmt.Println(prices)
+// 	fmt.Println(productNames)
+// 	fmt.Println(prices[0])
+
+// 	// Slicing
+// 	featuredPrices := prices[1:]
+// 	featuredPrices[1] = 330.50
+// 	highlightedPrices := featuredPrices[:1]
+
+// 	fmt.Println(highlightedPrices)
+// 	fmt.Println(prices)
+// 	fmt.Println(len(highlightedPrices), cap(highlightedPrices))
+
+// }
